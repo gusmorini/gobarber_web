5addcc90e901e2f8e6ff611e3e9a6ae4d4d33c12
Gustavo Morini <gustavommh@gmail.com>
2020-03-12 18:02:30 -0300

    api

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,22 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', { email, password });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Usuario não é prestador');
+    return;
+  }
+
+  yield put(signInSuccess(token, user));
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
