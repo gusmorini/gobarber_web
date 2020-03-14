cac363fd6881f05b76aaeedc07707b953667179c
Gustavo Morini <gustavommh@gmail.com>
2020-03-14 09:29:57 -0300

    sagaMonitor fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import './config/reactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
 
-import './config/reactotronConfig';
 import GlobalStyle from './styles/global';
 
 import store from './store';
